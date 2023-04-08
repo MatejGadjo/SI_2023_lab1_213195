101fe1d7e41726bbf2c082b64ea47b1b2f15feab
MatejGadjo <m.gadzovski@gmail.com>
2023-04-08 11:42:46 +0200

    Fix wrong condition in filterOddNumbers

--- SILab1.java
+++ SILab1.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class SILab1 {
     public static List<Integer> filterOddNumbers(List<Integer> list) {
-        List<Integer> result = new ArrayList<>();
-        for (Integer el : list) {
-            if (el % 2 == 0) {
-                result.add(el);
+        List<Integer> newList = new ArrayList<>();
+        for (Integer i : list) {
+            if (i % 2 == 0) {
+                newList.add(i);
             }
         }
-        return result;
+        return newList;
     }
 
     private static int sumEvenNumbers(List<Integer> list) {
